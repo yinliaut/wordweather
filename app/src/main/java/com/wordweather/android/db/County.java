4cd22ebc1f0bfbe3fd40ece5a05df7a2f6835288
RunAut <675512296@qq.com>
2018-06-21 20:55:53 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/wordweather/android/db/County.java
+++ app/src/main/java/com/wordweather/android/db/County.java
@@ -8,7 +8,7 @@ import org.litepal.crud.DataSupport;
 public class County extends DataSupport {
     private int id;
 
-    private String counttyName;
+    private String countyName;
 
     private String weatherId;
 
@@ -22,12 +22,12 @@ public class County extends DataSupport {
         this.id = id;
     }
 
-    public String getCounttyName() {
-        return counttyName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCounttyName(String counttyName) {
-        this.counttyName = counttyName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
